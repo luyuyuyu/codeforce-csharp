fa4438add75ac06fefa571711b72709cc0e0b792
鲈鱼 <lyu961109@gmail.com>
2018-11-24 21:09:49 +0800

    Updated by Cloud Studio , https://studio.dev.tencent.com

---
+++ A118.cs
@@ -0,0 +1,9 @@
+using System;
+
+class A118 {
+    public static void Main() {
+        var string1 = Console.ReadLine().ToLower();
+        var string2 = Console.ReadLine().ToLower();
+        Console.WriteLine(string1.CompareTo(string2));
+    }
+}
